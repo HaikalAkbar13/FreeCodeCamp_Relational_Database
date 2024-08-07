cc236f3c5118b12bffbe221e0259aa594710b17f
HaikalAkbar13 <haikalakbar10k@gmail.com>
2024-08-07 09:18:04 +0800

    Update five.sh

--- Learn Bash Scripting by Building 5 Programs/five.sh
+++ Learn Bash Scripting by Building 5 Programs/five.sh
@@ -3,6 +3,6 @@
 # Program to run my other four programs
 
 ./questionnaire.sh
-./countdown.sh 3
+./countdown.sh 3 # the number represent second that we will start the countdown
 ./bingo.sh
 ./fortune.sh
